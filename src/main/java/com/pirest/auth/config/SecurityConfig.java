f2badd693af74d84331d7bd97b483fcc29966dba
Admin <Admin@host.docker.internal>
2018-11-20 08:33:32 +0700

    add permit all for /oauth/token request mapping

--- src/main/java/com/pirest/auth/config/SecurityConfig.java
+++ src/main/java/com/pirest/auth/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -38,7 +39,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().anonymous().disable().authorizeRequests().antMatchers("/api-docs/**").permitAll();
+		http.csrf().disable().anonymous().disable().authorizeRequests().antMatchers("/oauth/**").permitAll()
+				.antMatchers(HttpMethod.OPTIONS).permitAll();
 	}
 
 	@Bean
