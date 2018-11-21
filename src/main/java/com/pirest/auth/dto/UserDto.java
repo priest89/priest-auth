686bc797f78ddd39557f71c729b1d28db8f8a398
Leo <dungct.hut@gmail.com>
2018-11-21 13:47:05 +0700

    add mapper

--- src/main/java/com/pirest/auth/dto/UserDto.java
+++ src/main/java/com/pirest/auth/dto/UserDto.java
@@ -1,5 +1,35 @@
 package com.pirest.auth.dto;
 
-public class UserDto {
+import java.io.Serializable;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class UserDto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String userName;
+
+	private String password;
+
+	private List<String> roles;
+
+	private List<String> permissions;
 }
