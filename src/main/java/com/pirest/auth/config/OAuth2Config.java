21cb7e318ed434d8049f32cb69fe65d604ac640b
Leo <dungct.hut@gmail.com>
2018-11-22 16:17:43 +0700

    back to jwt

--- src/main/java/com/pirest/auth/config/OAuth2Config.java
+++ src/main/java/com/pirest/auth/config/OAuth2Config.java
@@ -10,12 +10,14 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
-	static final String CLIEN_ID = "devglan-client-2";
+	static final String CLIEN_ID = "devglan-client-3";
 	static final String CLIENT_SECRET = "devglan-secret";
 	static final String GRANT_TYPE_PASSWORD = "password";
 	static final String AUTHORIZATION_CODE = "authorization_code";
@@ -33,34 +35,25 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 	@Autowired
 	private DataSource dataSource;
 
-//	@Bean
-//	public JwtAccessTokenConverter accessTokenConverter() {
-//		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-//		converter.setSigningKey("as466gf");
-//		return converter;
-//	}
-//
-//	@Bean
-//	public TokenStore tokenStore() {
-//		return new JwtTokenStore(accessTokenConverter());
-//	}
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		converter.setSigningKey("as466gf");
+		return converter;
+	}
 
 	@Bean
-	public JdbcTokenStore tokenStore() {
-		return new JdbcTokenStore(dataSource);
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
 	}
-	
+
 	@Override
 	public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
-
-		configurer.jdbc(dataSource).withClient(CLIEN_ID).secret(CLIENT_SECRET)
-				.authorizedGrantTypes(GRANT_TYPE_PASSWORD, AUTHORIZATION_CODE, REFRESH_TOKEN, IMPLICIT)
-				.scopes(SCOPE_READ, SCOPE_WRITE, TRUST).accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_SECONDS)
-				.refreshTokenValiditySeconds(FREFRESH_TOKEN_VALIDITY_SECONDS);
+		configurer.jdbc(dataSource);
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+		endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager).accessTokenConverter(accessTokenConverter());
 	}
 }
---
+++ src/main/java/com/pirest/auth/controller/AuthController.java
@@ -0,0 +1,8 @@
+package com.pirest.auth.controller;
+
+import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpoint;
+
+@FrameworkEndpoint
+public class AuthController {
+	
+}
