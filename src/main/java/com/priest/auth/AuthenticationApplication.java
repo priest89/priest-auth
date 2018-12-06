1828e7b62814d312b34d7ef29d15f432f1b27bd2
pirest89 <thang89@gmail.com>
2018-12-06 07:59:57 +0700

    spring cloud config

--- src/main/java/com/priest/auth/AuthenticationApplication.java
+++ src/main/java/com/priest/auth/AuthenticationApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableResourceServer
@@ -16,8 +19,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = { "com.priest.auth.repository" })
 @EnableTransactionManagement
 @EnableDiscoveryClient
-public class PriestAuthenticationApplication {
+public class AuthenticationApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(PriestAuthenticationApplication.class, args);
+		SpringApplication.run(AuthenticationApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
